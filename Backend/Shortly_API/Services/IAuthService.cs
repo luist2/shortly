66e8bc880612da4083a67b6add863aa9f0612717
luist29 <luistroncoso29@hotmail.com>
2025-10-14 19:29:26 -0300

    refactor: change RegisterAsync to return UserResponseDTO

--- Backend/Shortly_API/Services/IAuthService.cs
+++ Backend/Shortly_API/Services/IAuthService.cs
@@ -5,7 +5,7 @@ namespace Shortly_API.Services
 {
     public interface IAuthService
     {
-        Task<User?> RegisterAsync(UserDTO request);
+        Task<UserResponseDTO?> RegisterAsync(UserDTO request);
         Task<TokenResponseDTO?> LoginAsync(UserDTO request);
         Task<TokenResponseDTO> RefreshTokensAsync(RefreshTokenRequestDTO request);
     }
