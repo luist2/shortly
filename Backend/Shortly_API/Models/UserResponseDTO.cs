d572b0e5fa3b89ea8140c45938af3871895f1980
luist29 <luistroncoso29@hotmail.com>
2025-10-14 19:28:33 -0300

    feat: add UserResponseDTO to return user data safely

---
+++ Backend/Shortly_API/Models/UserResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Shortly_API.Models
+{
+    public class UserResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+    }
+}
