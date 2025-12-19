e852f9c8d9b87ecd09377aa076940b02cf9d4f47
luist29 <luistroncoso29@hotmail.com>
2025-12-19 12:41:29 -0300

    feat(url-stats): read shortCode from route params

--- Frontend/shortly-frontend/src/app/features/url-stats/url-stats.component.ts
+++ Frontend/shortly-frontend/src/app/features/url-stats/url-stats.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-url-stats',
   templateUrl: './url-stats.component.html',
-  styleUrls: ['./url-stats.component.scss']
+  styleUrls: ['./url-stats.component.scss'],
 })
-export class UrlStatsComponent {
+export class UrlStatsComponent implements OnInit {
+  shortCode: string = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Obtener el parametro shortCode de la URL
+    this.shortCode = this.route.snapshot.paramMap.get('shortCode') || '';
+
+    if (this.shortCode) {
+      console.log(`Short code obtenido: ${this.shortCode}`);
+    }
+  }
 }
