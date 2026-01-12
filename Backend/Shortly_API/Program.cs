96f7f6f8bedb5a5dab35e2c0bdbf45307fb3955e
luist29 <luistroncoso29@hotmail.com>
2026-01-12 05:39:41 -0300

    fix: apply EF Core migrations on application startup

--- Backend/Shortly_API/Program.cs
+++ Backend/Shortly_API/Program.cs
@@ -58,6 +58,13 @@ builder.Services.AddScoped<IUrlShortenerService, UrlShortenerService>();
 
 var app = builder.Build();
 
+// Aplicar migraciones
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
